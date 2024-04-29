09b7c6f2ad49294f8b181425f348edbca5a58e3e
jhM00n <answlgns2056@kookmin.ac.kr>
2024-04-29 14:10:17 +0900

    update paginator

--- Lambda/Paginator/lambda_function.py
+++ Lambda/Paginator/lambda_function.py
@@ -14,6 +14,7 @@ table = dynamodb.Table(f'{prefix}DynamoDB')
 def lambda_handler(event, context):
     # API를 호출한 유저의 닉네임을 가져옴
     username = event["queryStringParameters"]["UserName"]
+
     # 인스턴스 정보를 관리하는 DB에 존재하는 elements를 가져옴
     response = table.scan(
         FilterExpression=boto3.dynamodb.conditions.Attr('UserName').eq(username)
