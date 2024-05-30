a31e1e7789ea296d8c810dac9a2503c0be8362e9
jhM00n <answlgns2056@kookmin.ac.kr>
2024-05-30 13:32:31 +0900

    update Controller

--- Lambda/Controller/lambda_function.py
+++ Lambda/Controller/lambda_function.py
@@ -1,81 +1,48 @@
 import boto3
 import json
+from boto3.dynamodb.conditions import Attr
 from variables import *
 
-ec2_client = boto3.client('ec2')
+# DynamoDB 서비스 리소스 생성
 dynamodb = boto3.resource('dynamodb')
-elbv2_client = boto3.client('elbv2')
 
+# 쿼리할 테이블 선택
 table = dynamodb.Table(f'{prefix}DynamoDB')
 
 def lambda_handler(event, context):
-    # 조작의 대상 인스턴스 ID, 어떤 조작(Stop, Start, Terminate)을 가할것인지에 대한 정보를 가져옴
-    control_type = event["queryStringParameters"]["ControlType"]
-    instance_id = event["queryStringParameters"]["InstanceId"]
-    # 조작 수행
-    response = None
-    if control_type == 'start':
-        response = ec2_client.start_instances(InstanceIds=[instance_id])
-        responsedb = table.update_item(
-            Key={
-                'InstanceId': instance_id,  # 예: 'UserID': '12345'
-            },
-            UpdateExpression='SET #status = :newStatus',
-            ExpressionAttributeNames={
-                '#status': 'Status',
-            },
-            ExpressionAttributeValues={
-                ':newStatus': 'running'
-            },
-            ReturnValues="UPDATED_NEW"  # 업데이트된 속성의 새로운 값 반환
-        )
-    elif control_type == 'stop':
-        response = ec2_client.stop_instances(InstanceIds=[instance_id])
-        responsedb = table.update_item(
-            Key={
-                'InstanceId': instance_id,  # 예: 'UserID': '12345'
-            },
-            UpdateExpression='SET #status = :newStatus',
-            ExpressionAttributeNames={
-                '#status': 'Status',
-            },
-            ExpressionAttributeValues={
-                ':newStatus': 'stopped'
-            },
-            ReturnValues="UPDATED_NEW"  # 업데이트된 속성의 새로운 값 반환
-        )
-    elif control_type == 'terminate':# Target Group 찾기
-        response = elbv2_client.describe_target_groups()
-        target_groups_to_delete = []
+    try:
+        # API를 호출한 유저의 닉네임을 가져옴
+        body = json.loads(event['body'])
+        username = body["UserName"].strip()
 
-        for tg in response['TargetGroups']:
-            # 각 Target Group에 등록된 대상 확인
-            targets = elbv2_client.describe_target_health(TargetGroupArn=tg['TargetGroupArn'])
-            for target in targets['TargetHealthDescriptions']:
-                if target['Target']['Id'] == instance_id:
-                    target_groups_to_delete.append(tg['TargetGroupArn'])
-                    break
-
-        # Target Group 삭제
-        for tg_arn in target_groups_to_delete:
-            # 해당 Target Group을 사용하는 Listener 찾기
-            listeners = elbv2_client.describe_listeners(LoadBalancerArn='YOUR_LOAD_BALANCER_ARN')
-            for listener in listeners['Listeners']:
-                for action in listener['DefaultActions']:
-                    if action['TargetGroupArn'] == tg_arn:
-                        # Listener 삭제
-                        elbv2_client.delete_listener(ListenerArn=listener['ListenerArn'])
-            # Target Group 삭제
-            elbv2_client.delete_target_group(TargetGroupArn=tg_arn)
-        response = ec2_client.terminate_instances(InstanceIds=[instance_id])
-        responsedb = table.delete_item(
-            Key={
-                'InstanceId': instance_id,
+        # DynamoDB에서 필터 조건을 사용하여 항목 조회
+        response = table.scan(
+            FilterExpression=Attr('UserName').eq(username)
+        )
+        items = response['Items']
+        print("필터링된 항목: ", items)
+        
+        # 변환된 데이터 구조로 변환
+        page = [
+            {
+                "servername": item.get('InstanceName'),
+                "id": item.get('InstanceId'),
+                "type": item.get('InstanceType'),
+                "isRunning": item.get('isRunning'),
+                "address": item.get('IpAddress'),
+                "isWeb": item.get("isWeb"),
+                "port": item.get("port"),
+                "SpotReqId": item.get("SpotReqId")
             }
-        )
-    print(response)
-    print(responsedb)
-    return {
-        "statusCode": 200,
-        "message": json.dumps(response)
-    }
+            for item in items
+        ]
+        
+        return {
+            "statusCode": 200,
+            "body": json.dumps(page)
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
